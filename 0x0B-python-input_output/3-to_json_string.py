a6ec13f1fa1131186c925f98f3789fb8505e3c46
IB-1414 <ibresguru@gmail.com>
2023-01-11 17:02:04 -0800

    all tasks

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Defines a string-to-JSON function."""
+import json
+
+
+def to_json_string(my_obj):
+    """Return the JSON representation of a string object."""
+    return json.dumps(my_obj)
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+S
